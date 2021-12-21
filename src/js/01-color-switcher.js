d4cc3f96d41398613a3dd6d727d536248cdb2b95
Shynal-Nazar <87614426+Shynal-Nazar@users.noreply.github.com>
2021-12-21 09:55:09 +0200

    Add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+import '../css/common.css';
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+startBtn.addEventListener('click', startRandomColor);
+stopBtn.addEventListener('click', stopRandomColor);
+
+let timerId = null;
+
+function startRandomColor() {
+  startBtn.setAttribute('disabled', 'true');
+  stopBtn.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+}
+
+function stopRandomColor() {
+  stopBtn.setAttribute('disabled', 'true');
+  startBtn.removeAttribute('disabled');
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
