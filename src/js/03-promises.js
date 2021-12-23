62af30a83dce72c68284e5b51c18f99e6c37a61f
Shynal Nazar <Pro100nazar@i.ua>
2021-12-23 19:13:13 +0200

    Task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  let delay = Number(formElements.delay.value);
+  const step = Number(formElements.step.value);
+  const amount = Number(formElements.amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        }, delay);
+      });
+    delay += step;
+  }
+  event.currentTarget.reset();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      // Fulfill
+      resolve({ position, delay });
+    } else {
+      // Reject
+      reject({ position, delay });
+    }
+  });
 }
